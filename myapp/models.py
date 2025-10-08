0cdcbfb37a54e3e49557ec95373d26f90e8f452f
NicoBoost <nm.aguayo@hotmail.com>
2025-10-08 03:15:04 -0300

    Proyecto Django CRUD inicial

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Project(models.Model):
+class myapp(models.Model):
     name = models.CharField(max_length=200)
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(myapp, on_delete=models.CASCADE)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    marca = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,38 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import myapp
+from .forms import ProductoForm
 
-# Create your views here.
-def hello(request):
-    return HttpResponse('<h2>Hello World<h2>')
+def index(request):
+    return render(request, 'myapp/index.html')
 
-def about(request):
-    return HttpResponse('about')
+def listar_productos(request):
+    myapp = myapp.objects.all()
+    return render(request, 'myapp/listar.html', {'myapp': myapp})
+
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'myapp/crear.html', {'form': form})
+
+def editar_producto(request, id):
+    producto = get_object_or_404(myapp, id=id)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'myapp/editar.html', {'form': form, 'producto': producto})
+
+def eliminar_producto(request, id):
+    producto = get_object_or_404(myapp, id=id)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('listar_productos')
+    return render(request, 'myapp/eliminar.html', {'producto': producto})
